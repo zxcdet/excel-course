e1d806710735157946c22b2a6f9be1fced73ab2d
Mykhailo Kisil <jallyface@gmail.com>
2021-07-23 15:57:54 +0300

    Finish redux and table logic

--- src/index.js
+++ src/index.js
@@ -4,8 +4,23 @@ import {Toolbar} from "@/components/toolbar/Toolbar";
 import {Formula} from "@/components/formula/Formula";
 import {Table} from "@/components/table/Table";
 import {Header} from "@/components/header/Header";
+import {createStore} from "@core/createStore";
+import {rootReducer} from "@/redux/rootReducer";
+import {storage , debounce} from "@core/utils";
+import {initialState} from "@/redux/initialState";
+
+const store = createStore(rootReducer, initialState)
+
+const stateLisatener = debounce(state => {
+    console.log('App State: ', state)
+    storage('excel-state' , state)
+} , 300)
+
+
+store.subscribe(stateLisatener)
 const excel = new Excel('#app', {
-    components: [Header, Toolbar, Formula, Table]
+    components: [Header, Toolbar, Formula, Table],
+    store
 })
 
 excel.render()
